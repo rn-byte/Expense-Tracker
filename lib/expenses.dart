69adde45dee287171f7aaecb9c7025f25e636e1d
RNT4216 <ramthakur977@gmail.com>
2023-11-30 19:30:41 +0545

    Expenses file added

---
+++ lib/expenses.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: "Demo App",
-      theme: ThemeData(useMaterial3: true),
-      home: Scaffold(
-        backgroundColor: Colors.lightBlue,
-        appBar: AppBar(
-          title: const Text("My First App"),
-        ),
-        body: const Center(
-          child: Text("Hello !"),
-        ),
-      ),
+    const MaterialApp(
+      home: Expenses(),
     ),
   );
 }
